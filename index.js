5abc1d1f99d5fff4842950e66cfdc977640855b3
zhangyaochun <zhangyaochun@wandoujia.com>
2014-08-13 20:31:33 +0800

    init

---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = function(path, keys){
+
+    //support argv like --no-color and -- color
+    if (process.argv.indexOf('--no-color') > -1) {
+        return false;
+    }
+
+    //node c.js --color
+    //[ 'node', '/Users/***/c.js', '--color' ]
+    if (process.argv.indexOf('--color') > -1) {
+        return true;
+    }
+
+    if (!process.stdot.isTTY) {
+        return false;
+    }
+
+    if (process.platform === 'win32') {
+        return true;
+    }
+
+    if ('COLORTERM' in process.env) {
+        return true;
+    }
+
+    //test case: cygwin
+    return /^screen|^xterm|color|ansi|cygwin|linux/i.test(process.env.TERM);
+
+};
